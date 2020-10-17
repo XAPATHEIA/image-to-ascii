fe118e9dcd9ed0b9f046fd59f537233684956775
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-10-17 23:34:47 +0100

    image breakdown

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-import numpy
 from PIL import Image
 
+g_scale1 = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`. "
+g_scale_map = []
 
-# Defining the two gray-scale levels in descending strength.
-g_scale1 = r'$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"`". "'  # 70 levels.
-g_scale2 = r'@%#*+=-:. '  # 10 levels.
+for i in range(1, len(g_scale1)+1):
+    g_scale_map.append(g_scale1[-i])
 
-# open image and convert to grayscale
-image = Image.open('roadster.jpg').convert('L')
 
-# store dimensions
-W, H = image.size[0], image.size[1]
 
-# compute width of the tile
-w = W / cols
+def path_check():
+    try:
+        path = input("Input the path of the image you would like to convert:\n")
+    except FileNotFoundError:
+        print("Could not locate image.")
+    else:
+        return path
+
+
+# Open original image and convert to grayscale.
+o_img = Image.open('roadster.jpg').convert('RGB')          # change to path_check()
+W, H = o_img.size
+data = o_img.load
+
+pixel_data = o_img.getcolors(W * H)
+
+average_gs = []
+
+for ipc in pixel_data:
+    average_gs.append(round(sum(ipc[1]) / len(ipc[1])))
+
+for
+
+
