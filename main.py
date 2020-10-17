e55f504b3e01a2af8456d3f03cf4267bbfa54511
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-10-17 15:06:30 +0100

    update

---
+++ main.py
@@ -0,0 +1,5 @@
+import pillow
+
+# Defining the two gray-scale levels in descending strength.
+g_scale1 = r'$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"`". "'  # 70 levels.
+g_scale2 = r'@%#*+=-:. '  # 10 levels.
