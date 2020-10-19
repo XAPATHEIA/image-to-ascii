516ae0b8b17068fb0daee30c32cfd08e45a73943
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-10-19 11:35:44 +0100

    Function addition
    - resize function
    - grayscale function
    - path check function
    - user decided widths

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from PIL import Image
 
 g_scale1 = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`. "
-g_scale_map = []
-
-for i in range(1, len(g_scale1)+1):
-    g_scale_map.append(g_scale1[-i])
+g_scale_map = g_scale1[::-1]
 
 
+# Open image and convert to grayscale.
+def grayscale():
+    return Image.open('roadster.jpg').convert('L')  # change to path_check()
 
+
+# Check path eligibility.
 def path_check():
     try:
         path = input("Input the path of the image you would like to convert:\n")
@@ -17,18 +19,19 @@ def path_check():
         return path
 
 
-# Open original image and convert to grayscale.
-o_img = Image.open('roadster.jpg').convert('RGB')          # change to path_check()
-W, H = o_img.size
-data = o_img.load
-
-pixel_data = o_img.getcolors(W * H)
-
-average_gs = []
-
-for ipc in pixel_data:
-    average_gs.append(round(sum(ipc[1]) / len(ipc[1])))
-
-for
+def resize(image):
+    try:
+        new_width = int(input("Width of ASCII image: "))
+    except ValueError as V:
+        print(V)
+    else:
+        original_width, original_height = image.size
+        aspect_ratio = original_width / original_height
+        new_height = aspect_ratio * new_width
+        new_dimensions = (new_width, new_height)
+        return image.resize(new_dimensions)
 
 
+
+
+pixel_data = o_img.getdata()
